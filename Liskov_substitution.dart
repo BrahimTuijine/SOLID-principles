847aa39cc06f83a9b1f554455b08f8b74b423868
Brahim tuijine <brahimtuijine220@gmail.com>
2024-09-17 17:36:14 +0100

    implement Liskov substitution principle

---
+++ Liskov_substitution.dart
@@ -0,0 +1,59 @@
+//* BAD */
+
+// class UploadFileService {
+//   void uploadFile() {
+//     print('Parent class : upload file ...');
+//   }
+// }
+
+// class UploadImage extends UploadFileService {
+//   void uploadImage() {
+//     print('Image child class: upload image ...');
+//   }
+// }
+
+// class UploadVideo extends UploadFileService {
+//   void uploadImage() {
+//     print('Video child class: upload video ...');
+//   }
+// }
+
+//** Good */
+
+class UploadFileService {
+  void uploadFile() {
+    print('Parent class : upload file ...');
+  }
+}
+
+class UploadImage extends UploadFileService {
+  @override
+  void uploadFile() {
+    print('Image child class: upload image ...');
+  }
+}
+
+class UploadVideo extends UploadFileService {
+  @override
+  void uploadFile() {
+    print('Video child class: upload video ...');
+  }
+}
+
+void main() {
+  final data = <String>['video.mp4', 'image.png', 'file.pdf'];
+
+  late UploadFileService uploadFile;
+
+  for (var file in data) {
+    if (file.contains('.mp4')) {
+      uploadFile = UploadVideo();
+    } else if (file.contains('png')) {
+      uploadFile = UploadImage();
+    } else {
+      uploadFile = UploadFileService();
+    }
+
+    uploadFile.uploadFile();
+  }
+}
